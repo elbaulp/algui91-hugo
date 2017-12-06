68ce5651c59c8ec80f1865d25f9dd0b8c62e3858
Alejandro Alcalde <algui91@gmail.com>
2017-12-06 12:56:48 +0100

    Finally fixing minify error

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Stop script when an error occurs
+set -o errexit
+set -o pipefail
+set -o nounset
+# For debugging purposes
+set -o xtrace
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
